9b7d41d63dc0ddca1b8905f4fc9b12f3bbf1e9a3
zuramai <saugi.dev@gmail.com>
2025-02-09 14:16:29 +0700

    refactor: use handler struct

--- crates/pembantu_telegram/src/main.rs
+++ crates/pembantu_telegram/src/main.rs
@@ -4,47 +4,52 @@ use teloxide::dispatching::UpdateFilterExt;
 use teloxide::prelude::*;
 use dotenv::dotenv;
 use std::env;
+use std::sync::Arc;
 
 pub mod media_kind;
 pub mod command;
 pub mod conversation;
 pub mod updates;
 
-
-async fn answer_command(bot: teloxide::Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    log::info!("Replying to command");
-    let bot_kind = BotKind::OpenRouter(env::var("OPENROUTER_API").unwrap());
-    let model = env::var("AI_MODEL").unwrap();
-    let convo = conversation::Conversation::new(bot_kind, model);
-    convo.reply_command(bot, msg, cmd).await?;
-
-    Ok(())
+#[derive(Clone)]
+struct Handler {
+    conversation: conversation::Conversation,
+    bot: teloxide::Bot,
+    bot_username: String,   
 }
-async fn answer_replied_message(bot: teloxide::Bot, msg: Message) -> ResponseResult<()> {
-    let bot_kind = BotKind::OpenRouter(env::var("OPENROUTER_API").unwrap());
-    let model = env::var("AI_MODEL").unwrap();
-    let convo = conversation::Conversation::new(bot_kind, model);
-    let bot_username = env::var("BOT_USERNAME").expect("BOT_USERNAME should be set");
-    log::info!("Received message");
 
-    if let Some(reply_to_msg) =  msg.reply_to_message() {
-        if let Some(user) = reply_to_msg.from() {
-            if user.username.as_ref().unwrap() == &bot_username {
-                convo.reply_message(bot, msg).await?;
-            }
-        } 
+impl Handler {
+    async fn answer_command(&self, msg: Message, cmd: Command) -> ResponseResult<()> {
+        log::info!("Replying to command");
+        let result = self.conversation.reply_command(self.bot.clone(), msg, cmd).await;
+    
+        result
     }
-    else if msg.text().is_some() {
-        let text = msg.text().unwrap();
-
-        if text.starts_with("AI,") {
-            // replying to private chat
-            convo.reply_message(bot, msg).await?;
+    async fn answer_replied_message(&self, msg: Message) -> ResponseResult<()> {
+        log::info!("Received message");
+    
+        if let Some(reply_to_msg) =  msg.reply_to_message() {
+            if let Some(user) = reply_to_msg.from() {
+                if user.username.as_ref().unwrap() == &self.bot_username {
+                    let result = self.conversation.reply_message(self.bot.clone(), msg).await;
+                    return result
+                }
+            } 
         }
+        else if msg.text().is_some() {
+            let text = msg.text().unwrap();
+            
+            if text.starts_with("AI,") {
+                // replying to private chat
+                let result = self.conversation.reply_message(self.bot.clone(), msg).await;
+                return result
+            }
+        }
+    
+        Ok(())
     }
-
-    Ok(())
 }
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
@@ -53,10 +58,33 @@ async fn main() {
     log::info!("Starting bot..");
     
     let bot = teloxide::Bot::from_env();
+    
+    let bot_kind = BotKind::OpenRouter(env::var("OPENROUTER_API").unwrap());
+    let model = env::var("AI_MODEL").unwrap();
+    let bot_username = env::var("BOT_USERNAME").expect("BOT_USERNAME should be set");
+    let conversation = conversation::Conversation::new(bot_kind, model);
 
+    let handler = Handler {
+        conversation,
+        bot: bot.clone(),
+        bot_username
+    };
+
+    let handler_arc = handler.clone();
+    let handler_arc2 = handler_arc.clone();
     let handler = dptree::entry()
-        .branch(Update::filter_message().filter_command::<command::Command>().endpoint(answer_command))
-        .branch(Update::filter_message().endpoint(answer_replied_message));
+        .branch(Update::filter_message().filter_command::<command::Command>().endpoint(move |msg: Message, cmd: Command| {
+            let handler = handler_arc.clone();
+            async move {
+                handler.answer_command(msg.clone(), cmd.clone()).await
+            }
+        }))
+        .branch(Update::filter_message().endpoint(move |msg| {
+            let handler = handler_arc2.clone();
+            async move { 
+                handler.answer_replied_message(msg).await 
+            }
+        }));
     
     Dispatcher::builder(bot, handler).enable_ctrlc_handler().build().dispatch().await;
 }
